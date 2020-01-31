622c66682ded82e92288b720487b67246759ec29
unknown <shahnazajmeri@gmail.com>
2020-01-31 14:32:45 -0500

    comppnent structure

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.scss";
+import DirectoryMenu from "../../components/directory/diretcory-menu.component";
+const Homepage = () => (
+    <div className="homepage">
+        <DirectoryMenu />
+    </div>
+    
+)
+export default Homepage;
