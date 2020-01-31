3104f14121393cbd0dbbe83d9ab751bcfa2b365e
unknown <shahnazajmeri@gmail.com>
2020-01-31 11:44:07 -0500

    Initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Jackets</h1>
+                    <p>Text</p>
+                </div>               
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <p>Text</p>
+                </div>               
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Shoes</h1>
+                    <p>Text</p>
+                </div>               
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Women's</h1>
+                    <p>Text</p>
+                </div>               
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Men's</h1>
+                    <p>Text</p>
+                </div>               
+            </div>            
+        </div>
+    </div>
+    
+)
+export default Homepage;
