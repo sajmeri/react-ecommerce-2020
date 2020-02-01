285ccfbee989ad7e343d3cad2ea50201c3b3d4bb
unknown <shahnazajmeri@gmail.com>
2020-02-01 14:20:26 -0500

    Component preview and component Item

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import React from 'react';
 import {Switch, Route} from "react-router-dom";
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) =>{
-  console.log(props);
-  return(
-    <div>
-        <h1>Hats page</h1>
-  </div>
-  )
-}  
+import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
         
       </Switch>
     </div>
---
+++ src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+class ShopPage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            collection: SHOP_DATA
+        }
+    }
+    render(){
+        const {collection} = this.state;
+        return(
+            <div>
+                <h1>Collections</h1>
+                { collection.map(({id, ...otherCollectionProps}) =>
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                )}
+            </div>
+            
+        )
+    }
+}
+
+export default ShopPage;
